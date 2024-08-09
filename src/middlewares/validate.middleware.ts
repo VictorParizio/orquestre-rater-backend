bf4337cdb2acab595c648f73f4ae5bd7d1f5e043
Victor Parizio <victorparizio@hotmail.com>
2024-08-09 09:46:25 -0300

    feat: validate user register schema

---
+++ src/middlewares/validate.middleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from "express";
+import { Schema } from "joi";
+
+export const validate = (schema: Schema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+    
+    if (error) {
+      return res.status(400).json({
+        message: "Erro de validação",
+        details: error.details.map((detail) => detail.message),
+      });
+    }
+    
+    next();
+  };
+};
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller";
+import { userSchema } from "../schemas/user.schema";
+import { validate } from "../middlewares/validate.middleware";
 
 export const userRouter = Router();
 
-userRouter.post("/register", registerUser);
+userRouter.post("/register", validate(userSchema), registerUser);
