923a8eddec7d1e3b766f37e6006ae69223af2c6d
Victor Parizio <victorparizio@hotmail.com>
2024-08-09 07:55:43 -0300

    feat: server config + helmet

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import "express-async-errors";
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta http://localhost:${port}`);
+});
