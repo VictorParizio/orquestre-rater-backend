1c51339bdccfe00d4cc6aed627921382d4e5f109
Victor Parizio <victorparizio@hotmail.com>
2024-08-09 07:57:46 -0300

    feat: error middleware config

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import { limiter } from "./middlewares/limiter.middleware";
+import { errorMiddleware } from "./middlewares/error";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(helmet());
 app.use(limiter);
 app.use(express.json());
+app.use(errorMiddleware);
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta http://localhost:${port}`);
